8d28d8e45d3fd6f7c13f4f32c0e4328a73fa4771
Nate Keddie <nkeddie@paylocity.com>
2020-01-02 20:00:07 -0600

    Adding board render 2

---
+++ BoardCell.cs
@@ -0,0 +1,16 @@
+﻿namespace ChessKnight
+{
+  public class BoardCell
+  {
+    public BoardCell(int value, int x, int y)
+    {
+      Value = value;
+      X = x;
+      Y = y;
+    }
+
+    public int Value { get; }
+    public int X { get; }
+    public int Y { get; }
+  }
+}
